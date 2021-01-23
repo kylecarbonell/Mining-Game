2d70005d35501e49e5e7f81a5d7e6fcbf3b876d1
kylecarbonell <kylecarbonell13@gmail.com>
2021-01-23 12:09:35 -0800

    main file

--- Main.cpp
+++ Main.cpp
@@ -1,1 +1,22 @@
+#include "Mining.cpp"
 
+using namespace std;
+
+int main()
+{
+    int play;
+    do
+    {
+        int play = mainMenu();
+        system("cls");
+            if (play == 1)
+            {
+                mining();
+            }
+            else if (play == 3)
+            {
+                checkBackpack();
+            }
+    }while (play != 0);
+    cout << "Thanks for playing!" << endl;;
+}
